177c41431fe2b9da290cdd04ed30af2e79bfc991
senmdaniel <senmdaniel@hotmail.com>
2026-05-04 01:56:14 +0200

    Delete server.py

--- server.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, request, jsonify
-from convertdate import hebrew
-from yom_tov import get_yom_tov_day, is_yom_tov
-from zmanim.geo_location import GeoLocation
-from zmanim.zmanim_calendar import ZmanimCalendar
-import datetime
-import json
-import os

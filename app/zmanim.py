926b34037f052b1e3fa1c576163efed8ccbfc1d5
senmdaniel <senmdaniel@hotmail.com>
2026-05-05 11:05:03 +0200

    Update zmanim.py

--- app/zmanim.py
+++ app/zmanim.py
@@ -1,21 +1,13 @@
-import json
+def get_active_event(path):
+    import json
+    from datetime import datetime
 
+    try:
+        with open(path, "r") as f:
+            data = json.load(f)
+    except:
+        return None
 
-def is_yom_tov(path, date_obj):
-    with open(path, "r") as f:
-        data = json.load(f)
+    today = datetime.now().strftime("%Y-%m-%d")
 
-    for event in data.get("events", []):
-        if event["type"] == "yom_tov":
-            if event["date"] == str(date_obj):
-                return True
-
-    return False
-
-
-def calculate_times(date_obj):
-    # voorlopig dummy (later echte astronomische berekening)
-    return {
-        "plag_hamincha": "18:42",
-        "tzeis": "21:31"
-    }
+    return data.get(today)

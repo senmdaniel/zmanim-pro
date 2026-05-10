9543160f94d0a6bf5cbf4c4bd6d6e479d4898516
senmdaniel <senmdaniel@hotmail.com>
2026-05-10 15:16:41 +0200

    Create events.py

---
+++ app/data/events.py
@@ -0,0 +1,14 @@
+EVENTS = {
+    (1, 14): "12_erev_pesach",
+    (1, 15): "13_pesach_1",
+    (1, 16): "14_pesach_2",
+    (1, 21): "16_pesach_7",
+
+    (3, 6): "19_shavuot_1",
+
+    (7, 1): "2_rosh_hashanah_1",
+    (7, 10): "5_yom_kippur",
+
+    (7, 15): "7_sukkot_1",
+    (7, 23): "11_simchat_torah",
+}

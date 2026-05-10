851deaf7b82d72b008a20d72d996fcd97d8ec46a
senmdaniel <senmdaniel@hotmail.com>
2026-05-10 20:45:22 +0200

    Update hebrew_calendar.py

--- app/core/hebrew_calendar.py
+++ app/core/hebrew_calendar.py
@@ -1,5 +1,21 @@
 from convertdate import hebrew
 
+print("INPUT:", gregorian_date)
+print("HEBREW:", h_year, h_month, h_day)
+
+
+def get_hebrew_date(gregorian_date):
+    h_year, h_month, h_day = hebrew.from_gregorian(
+        gregorian_date.year,
+        gregorian_date.month,
+        gregorian_date.day
+    )
+
+    return {
+        "hebrew_year": h_year,
+        "hebrew_month": h_month,
+        "hebrew_day": h_day
+    }
 
 def get_hebrew_date(gregorian_date):
     h_year, h_month, h_day = hebrew.from_gregorian(

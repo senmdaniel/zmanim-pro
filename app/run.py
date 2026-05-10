724a9cfeb13087211b16293f727aa1bc62a713be
senmdaniel <senmdaniel@hotmail.com>
2026-05-10 20:26:22 +0200

    Update run.py

--- app/run.py
+++ app/run.py
@@ -1,4 +1,9 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
 from app.api import app
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
